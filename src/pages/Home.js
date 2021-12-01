bab1135054694fe13872bc973f4441875ad0e14a
elentuuk <69905873+elentuuk@users.noreply.github.com>
2021-12-01 22:17:55 +0800

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Header from '../components/Header';
+// import Movies from '../components/Movies';
+import Footer from '../components/Footer';
+// import Recipe from '../components/Recipe';
+
+
+const Home = () => {
+  return(
+    <>
+      <Navigation />
+      <Header />
+      {/* <Movies /> */}
+      <Footer />
+      {/* <Recipe /> */}
+    </>
+  );
+}
+
+export default Home;
